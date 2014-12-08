f5c8f9de107de0cf118f8c67992643177f9667e9
d378p <kimmld01@students.ipfw.edu>
2014-12-08 04:07:41 -0500

    Rev4 INIT not done yet

--- DataBase.py
+++ DataBase.py
@@ -2,41 +2,176 @@
 
 import MySQLdb
 
-class DataBase:  
+class DataBase:        
     
-    
-    def createUser(self,username, password, email): 
-        
-        validity='TRUE'       
-        return validity
- 
- 
-    def userExists(self, data):       
    
-        
-        validity=True       
+    def createUser(self, username, password, email):
+        # connect        
+        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="", db="ourmusic")
+        # set cursor
+        cursor = db.cursor()                 
+        exists = self.userExists(username, password, email)        
+        if exists != True:
+            # insert into db       
+            sql = "INSERT INTO LoginList (username, password, email) VALUES ('%s','%s','%s')" % (username, password, email);       
+            # Execute the SQL command
+            cursor.execute(sql)
+            # create user table
+            sql = "CREATE TABLE %s (id INT NOT NULL AUTO_INCREMENT,friendUsername VARCHAR(40) NOT NULL,friendFirst VARCHAR(40) NOT NULL,friendLast VARCHAR(40) NOT NULL,PRIMARY KEY ( id ));" % (username);
+            # Execute the SQL command
+            cursor.execute(sql)            
+            # commit your changes
+            db.commit()           
+            exist = True         
+             
+        elif exists == True:
+            exist = True;#should be false
+         
+        db.close()   
+            
+        validity = '{"validity":%s,"subject":"NEW_USER","type":"RESPONSE"}'% (exist)         
         return validity
- 
+    
+    def userExists(self, username, password, email):
+            # connect
+            db = MySQLdb.connect(host="localhost", port=3306, user="root",passwd="",db="ourmusic")
+               
+            cursor = db.cursor()
+                         
+            sql = "SELECT username s, count(*) FROM LoginList where username='%s' and password='%s'and email='%s';" % (username,password,email)
+                 
+            str1=""
+            try:
+                # Execute the SQL command
+                cursor.execute(sql)           
+            
+                # Fetch all the rows in a list of lists.
+                results = cursor.fetchall()               
+                data = ''
+                totalData = ''  
+                for list in results:               
+                    for number in list:                                      
+                        data = data+str(number)+"\n"                                                  
+                        totalData = data            
+                        #how to tokenize string
+                        tokens = totalData.split('\n')
+                        str1 = str(tokens[0])
+                        print str1                       
+            
+            except:
+                print "Error: unable to fecth data"
+
+                # disconnect from server
+                db.close()
+       
+            if str1 == username:
+                validity = True
+            else:
+                validity = False
+        
+            #print validity      
+            return validity
+        
+    def userExists2(self, username, password):
+            # connect
+            db = MySQLdb.connect(host="localhost", port=3306, user="root",passwd="",db="ourmusic")
+               
+            cursor = db.cursor()
+                         
+            sql = "SELECT username s, count(*) FROM LoginList where username='%s' and password='%s';" % (username,password)
+                 
+            str1=""
+            try:
+                # Execute the SQL command
+                cursor.execute(sql)           
+            
+                # Fetch all the rows in a list of lists.
+                results = cursor.fetchall()               
+                data = ''
+                totalData = ''  
+                for list in results:               
+                    for number in list:                                      
+                        data = data+str(number)+"\n"                                                  
+                        totalData = data            
+                        #how to tokenize string
+                        tokens = totalData.split('\n')
+                        str1 = str(tokens[0])
+                        print str1                       
+            
+            except:
+                print "Error: unable to fecth data"
+
+                # disconnect from server
+                db.close()
+       
+            if str1 == username:
+                validity = True
+            else:
+                validity = False
+        
+            #print validity      
+            return validity
+        
+    def checkCredentials(self, username, password):                
+        exists = self.userExists2(username, password)                
+        if exists==True:
+            validity = '{"validity":true}'
+        else:
+            validity = '{"validity":false}'                           
+        return validity
+    
+    def getSongs(self,username):
+        # connect       
+        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="",db="ourmusic")        
+        cursor = db.cursor()
+                         
+        sql = "SELECT Song s FROM SongList WHERE username IN ( '%s' )" % (username); 
+                
+        try:
+            # Execute the SQL command
+            cursor.execute(sql)           
+            # Fetch all the rows in a list of lists.
+            results = cursor.fetchall()
+            
+            data = ''
+            totalData = ''  
+            for list in results:               
+                for number in list:                                      
+                    data = data+str(number)+"\n" 
+                    #data = data+str(number)+" "                               
+            totalData = data
+            
+            #how to tokenize string
+            tokens = totalData.split('\n')
+            str1 = str(tokens[0])+" "+str(tokens[1])
+            print str1           
+            
+        except:
+            print "Error: unable to fecth data"
+
+            # disconnect from server
+            db.close() 
+    
  
       
     def setDB(self, data):
         # connect        
-        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="",db="ourmusic")
+        db = MySQLdb.connect(host="localhost", port=3306, user="root",passwd="", db="ourmusic")
 
-        #set cursor
+        # set cursor
         cursor = db.cursor()        
         
-        #tokenize data
+        # tokenize data
         str = data;
-        #split first string value
-        friend =  str.split(' ', 1 )[0];
-        song =  str.split(' ', 1 )[1];
+        # split first string value
+        friend = str.split(' ', 1)[0];
+        song = str.split(' ', 1)[1];
         
-        #print data
-        #print friend
-        #print song
+        # print data
+        # print friend
+        # print song
         
-        #insert into db       
+        # insert into db       
         sql = "INSERT INTO SongList (friend, song) VALUES ('%s', '%s')" % (friend, song);
        
         # Execute the SQL command
--- Server.py
+++ Server.py
@@ -1,5 +1,5 @@
 
-#import DataBase
+import DataBase
 import SocketServer
 import json
 
@@ -10,62 +10,55 @@ import json
 class TestTCPHandler(SocketServer.StreamRequestHandler):
     
     
-    def handle(self): 
-        
-        #db = DataBase.DataBase()      
-        
+    #server   
+    def handle(self):
+        #get database
+        database = DataBase.DataBase()        
         #get data
-        self.data = self.rfile.readline()
-        
+        self.data = self.rfile.readline()        
         decoded = json.loads(self.data)
- 
+        username = "" 
         # pretty printing of json-formatted string
-        print json.dumps(decoded, sort_keys=True, indent=4)
- 
-        print "JSON parsing example: ", decoded["subject"]
+        #print json.dumps(decoded, sort_keys=True, indent=4) 
+        #print "subject = ", decoded["subject"]
+        subject=decoded["subject"]
+               
+        #print subject == 'NEW_USER'         
+        if subject == 'NEW_USER':
+                
+                username = decoded["username"];
+                #print 'username = '+username         
+                password = decoded["password"];
+                #print 'password = '+password               
+                email = decoded["email"];
+                #print 'email = '+ email
+                
+                validity=database.createUser(username, password, email);       
+                #print 'validity = '+validity
         
-        
-          
-        
-#         #trim end off data
-#         self.data = self.data[2:-3]        
-#         print self.data
-#                 
-#         keyAndValue =  self.data.split(',')[1];
-#         subject = keyAndValue.split(':')[1];
-#         print 'subject = '+subject
-#         
-#         print subject == 'NEW_USER'
-#         
-#         if subject == 'NEW_USER':
-#                 keyAndValue =  self.data.split(',')[2];
-#                 username = keyAndValue.split(':')[1];
-#                 print 'username = '+username
-#         
-#                 keyAndValue =  self.data.split(',')[3];
-#                 password = keyAndValue.split(':')[1];
-#                 print 'password = '+password
-#         
-#                 keyAndValue =  self.data.split(',')[4];
-#                 email = keyAndValue.split(':')[1];
-#                 print 'email = '+ email
-#                 
-#                 validity = db.createUser(username, password, email)
-        validity='{"validity":true,"subject":"NEW_USER","type":"RESPONSE"}'
-                  
-        print 'validity = '+validity
-#         
-#                 #must convert result to string
-#                 #before writing
-        self.wfile.write(str(validity))
-#      
-     
+        elif subject == 'LOGIN':
+                
+                username = decoded["username"];
+                print 'username = '+username         
+                password = decoded["password"];
+                print 'password = '+password  
+                
+                validity=database.checkCredentials(username, password);       
+                print 'validity = '+validity
+                
+        elif subject == 'INIT':
+                
+                validity=database.getSongs(username);               
+         
+        #must convert result to string
+        #before writing    
+        self.wfile.write(str(validity))         
 
 if __name__== "__main__":
     HOST, PORT = "localhost", 9999   
-
+    
     server = SocketServer.TCPServer((HOST, PORT), TestTCPHandler) 
-
+    print 'Waiting on connection:'
     server.serve_forever()
     
    
